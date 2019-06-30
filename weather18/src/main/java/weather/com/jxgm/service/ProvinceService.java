6491fed375a05b6c708cef441971e2faada3158c
myhat123 <myhat123@gmail.com>
2019-06-30 22:28:31 +0800

    初步实现spring ioc自动装配

---
+++ weather18/src/main/java/weather/com/jxgm/service/ProvinceService.java
@@ -0,0 +1,46 @@
+package com.jxgm.service;
+
+import java.util.List;
+import javax.sql.DataSource;
+
+import com.jxgm.dao.*;
+import com.jxgm.entities.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProvinceService implements IProvinceService {
+    @Autowired 
+    PlainProvDao provDao;
+    @Autowired 
+    PlainCityDao cityDao;
+    
+    @Autowired
+    public ProvinceService(PlainProvDao provDao, PlainCityDao cityDao) {
+        this.provDao = provDao;
+        this.cityDao = cityDao;
+    }
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        provDao.setDataSource(dataSource);
+        cityDao.setDataSource(dataSource);
+    }
+
+    public List<Province> findAll() {
+        List<Province> provinces = provDao.findAll();
+        
+        for (Province p: provinces) {
+            String id = p.getId();
+            List<City> cities = cityDao.findByPid(id);
+            p.setCities(cities); 
+        }  
+        return provinces;
+    }
+
+    public List<Province> findAllWithCities() {
+        List<Province> prov = provDao.findAllWithCities();
+        return prov;
+    }    
+}
